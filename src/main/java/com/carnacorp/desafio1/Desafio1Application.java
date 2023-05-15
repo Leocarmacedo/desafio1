76ad1947c4f8bf25262def00bb972d2d4e5fa832
Leocarnauba <leocarmacedo@hotmail.com>
2023-05-15 15:31:15 -0300

    Finishing application

--- src/main/java/com/carnacorp/desafio1/Desafio1Application.java
+++ src/main/java/com/carnacorp/desafio1/Desafio1Application.java
@@ -1,20 +1,34 @@
 package com.carnacorp.desafio1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.carnacorp.desafio1.entities.Order;
+import com.carnacorp.desafio1.services.OrderService;
 
 @SpringBootApplication
-public class Desafio1Application implements CommandLineRunner{
+@ComponentScan({ "com.carnacorp" })
+public class Desafio1Application implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Desafio1Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Ola mundo!");
 		
+		Order order = new Order(1309, 95.9, 0.00);
+		
+		System.out.println("Pedido código " + order.getCode());
+		System.out.println("Valor total: R$ " + orderService.total(order));
+
 	}
 
 }
